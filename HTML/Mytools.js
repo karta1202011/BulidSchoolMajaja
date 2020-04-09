38af5b26334dcfb93c626d3166c7bef3c3f39f38
karta1202011 <61822685+karta1202011@users.noreply.github.com>
2020-04-09 19:40:43 +0800

    4-9

--- HTML/Mytools.js
+++ HTML/Mytools.js
@@ -1,6 +1,13 @@
-function $g (selector){
-    return document.querySelector(selector);
+function $g(selector) {
+    //判斷是否為id selector
+    if (selector.includes('#') && !selector.includes(' ')) {
+        //回傳Element
+        return document.querySelector(selector);
+    }
+    //回傳NodeList集合
+    let nodelist = document.querySelectorAll(selector);
+    return nodelist.length == 1 ? nodelist[0] : nodelist;
 }
-export {$g};
-
-
+function $c(selector) {
+    return document.createElement(selector);
+}
